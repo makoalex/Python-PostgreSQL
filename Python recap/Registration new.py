d3e1ff45d9e7e9097811abe5023b9bfd493eb55b
Alexandra Macovei <macovei.a2@gmail.com>
2020-04-01 21:52:21 +0200

    added methods and redid the registration system with saving and loading of the file

--- Python recap/Registration new.py
+++ Python recap/Registration new.py
@@ -1,60 +1,81 @@
+import pickle
+
 class Student:
     student_list = []
 
-    def __init__(self, student, mark):
-        self.student = student
-        self.mark = mark
-
-    def create_student(self, student):
-        student= input('please enter your name\n').lower().strip()
-        self.student = {'Name': student,
-                        'Marks': []}
-
-
-class Registration(Student):
     def __init__(self):
-        super().__init__()
+        name = input('please enter your name\n').lower().strip()
+        self.student = {'Name': name,
+                   'Marks': []}
+
+
+
+class Registration:
+    def __init__(self):
+        pass
+
 
     def adding_student(self):
         return Student.student_list.append(self.adding_marks)
 
-    def adding_marks(self, mark):
-        self.student['Marks'].append(mark)
-        return self.student
+    def adding_marks(self,mark, student):
+        student.student['Marks'].append(mark)
+        return student
 
-    def calculate_average(self):
-        if len(self.student['Marks']) == 0:
+    def calculate_average(self, student):
+        if len(student.student['Marks'])== 0:
             raise ValueError("can't average with 0 values")
-        total = sum(self.student['Marks'][0])
-        average = total / len(self.student['Marks'])
+        total = sum(student.student['Marks'])
+        average = total / len(student.student['Marks'])
         return average
 
-    def student_details(self):
-        return '{} and the average is {}'.format(self.student['Name'], self.calculate_average())
+    def student_details(self, student):
+        return '{} and the average is {}'.format(student.student['Name'], self.calculate_average(student))
 
     def getting_all_list(self):
-        for element, self.student in enumerate(Student.student_list):
+        for element, student in enumerate(Student.student_list):
             print('ID: {}'.format(element))
-            print(self.student_details())
+            print(self.student_details(student))
+
+    def save_students_to_file(self, name):
+        pickle.dump(Student.student_list, open(name, "wb"))
+
+    def open_students_from_file(self, name):
+        Student.student_list = pickle.load(open(name,"rb"))
 
     def menu_settings(self):
-        selection = input("Enter:\n"
-                          "'p' to get the student list \n"
-                          "'s'to add a new  student,\n"
-                          "'a' to add a new mark to a student,\n"
-                          "'q' to quit\n"
-                          "Enter selection:\n")
-        # while selection != 'q':
-        if selection == 'p':
-            print(self.getting_all_list())
-        elif selection == 's':
-            print(Student.student_list.append(self.adding_student()))
-        elif selection == 'a':
-            student_id = int(input("Enter the student id to add a mark to:\n"))
-            self.student = Student.student_list[student_id]
-            new_mark = int(input("Enter the new mark to be added"))
-            self.adding_marks(new_mark)
+        keep_running = True
+        while keep_running:
+            selection = input("Enter:\n"
+                              "'p' to get the student list \n"
+                              "'s'to add a new  student,\n"
+                              "'a' to add a new mark to a student,\n"
+                              "'q' to quit\n"
+                              "'l' to load\n"
+                              "'w' to save\n"
+                              "Enter selection:\n")
+            if selection == 'p':
+                print(self.getting_all_list())
+            elif selection =='s':
+                print(Student.student_list.append(Student()))
+            elif selection == "w":
+                name = input("Enter filename")
+                self.save_students_to_file(name)
+                print("File stored")
+            elif selection == "l":
+                name = input("Enter filename")
+                self.open_students_from_file(name)
+                print("File loaded")
+            elif selection == 'a':
+                student_id = int(input("Enter the student id to add a mark to:\n"))
+                student = Student.student_list[student_id]
+                new_mark = float(input("Enter the new mark to be added"))
+                self.adding_marks(new_mark,student)
+            elif selection == 'q':
+                keep_running = False
+
+
 
 
 s = Registration()
-print(s.menu_settings())
+s.menu_settings()
