2c8d503255e4e474808e21af6571bb880229961d
Alexandra Macovei <macovei.a2@gmail.com>
2020-04-21 11:25:35 +0200

    classmethod to load from the database

--- postgresql/project/App.py
+++ postgresql/project/App.py
@@ -1,4 +1,5 @@
 from User import User
-my_first= User('Mako@gmail.com', 'Mako', 'Chan', None)
-my_first.save_to_db()
+# my_first= User('Mako@gmail.com', 'Mako', 'Chan', None)
+# my_first.save_to_db()
+print(User.load_from_db('Mako@gmail.com'))
 
--- postgresql/project/User.py
+++ postgresql/project/User.py
@@ -8,9 +8,21 @@ class User:
         self.last_name = last_name
         self.id = id
 
+    def __repr__(self):
+        return 'User-{}.'.format(self.email)
+
     def save_to_db(self):
         with psycopg2.connect(database="Learning", user='postgres', password='karenina', host='localhost',
                               port=5433) as connection:
             with connection.cursor() as cursor:
                 cursor.execute('Insert into Users (Email, First_Name, Last_name) values (%s, %s, %s)',
                                (self.email, self.first_name, self.last_name))
+
+    @classmethod
+    def load_from_db(cls, email):
+        with psycopg2.connect(database='Learning', user='postgres', password='karenina', host='localhost',
+                              port=5433) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('Select* from users where Email = %s', (email,))
+                data = cursor.fetchone()
+                return 'Email-{},\nFirst Name-{}, \nLast Name-{}\nID-{}'.format(data[1], data[2], data[3], data[0])
