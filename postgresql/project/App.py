38db4e35b110f81d4c964287ab33f2cdf1ec323b
Alexandra Macovei <macovei.a2@gmail.com>
2020-04-21 08:54:32 +0200

    more project

---
+++ postgresql/project/App.py
@@ -0,0 +1,4 @@
+from User import User
+my_first= User('Mako@gmail.com', 'Mako', 'Chan', None)
+my_first.save_to_db()
+
--- postgresql/project/User.py
+++ postgresql/project/User.py
@@ -1,6 +1,16 @@
+import psycopg2
+
+
 class User:
     def __init__(self, email, first_name, last_name, id):
         self.email = email
-        self.first_name= first_name
-        self.last_name= last_name
-        self.id= id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id = id
+
+    def save_to_db(self):
+        with psycopg2.connect(database="Learning", user='postgres', password='karenina', host='localhost',
+                              port=5433) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('Insert into Users (Email, First_Name, Last_name) values (%s, %s, %s)',
+                               (self.email, self.first_name, self.last_name))
