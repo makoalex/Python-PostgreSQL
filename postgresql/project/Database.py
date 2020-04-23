c371ee931dc83264e0ca483a3365d6e87a4a92df
Alexandra Macovei <macovei.a2@gmail.com>
2020-04-23 09:01:00 +0200

    changing Coonection pool class for a w'with' statement

--- postgresql/project/Database.py
+++ postgresql/project/Database.py
@@ -1,10 +1,23 @@
 from psycopg2 import pool
 
 my_connection = pool.SimpleConnectionPool(
-    1, 2,
+    1, 1,
     database='Learning',
     user='postgres',
     password='karenina',
     host='localhost',
     port=5433
 )
+
+
+class ConnectionFromPool:
+    def __init__(self):
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = my_connection.getconn()
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        return my_connection.putconn(self.connection)
