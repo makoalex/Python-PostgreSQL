77a49857531352ea2f3d7c227a39311c5f2293d9
Alexandra Macovei <macovei.a2@gmail.com>
2020-05-03 11:48:49 +0200

    connection to app with success via oauth2

--- Python -Web-and-APIs/Twitter/login.py
+++ Python -Web-and-APIs/Twitter/login.py
@@ -1,6 +1,8 @@
-import constants
-import oauth2
 import urllib.parse as urlparse
+
+import oauth2
+
+import constants
 
 consumer = oauth2.Consumer(constants.CLIENT_KEY, constants.CLIENT_SECRET)
 client = oauth2.Client(consumer)
@@ -11,6 +13,11 @@ request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 print('Please fallow the site  in your browser')
 print('{}?oauth_token={}'.format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
 
-oauth_verifier= input('please enter the pin\n')
+oauth_verifier = input('please enter the pin\n')
 token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
 token.set_verifier(oauth_verifier)
+client = oauth2.Client(consumer, token)
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+authorized_token = oauth2.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
+authorized_client = oauth2.Client(consumer,authorized_token )
