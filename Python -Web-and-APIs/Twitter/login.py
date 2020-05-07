38558776396e50b27797c329212b2457540f6607
Alexandra Macovei <macovei.a2@gmail.com>
2020-05-07 09:07:00 +0200

    checking for users

--- Python -Web-and-APIs/Twitter/login.py
+++ Python -Web-and-APIs/Twitter/login.py
@@ -1,48 +1,57 @@
 import json
 import urllib.parse as urlparse
 from pprint import pprint
+
 import oauth2
 
 import constants
-from user import User
 from database import Database
+from user import User
 
-consumer = oauth2.Consumer(constants.CLIENT_KEY, constants.CLIENT_SECRET)
-client = oauth2.Client(consumer)
-response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
-if response.status != 200:
-    print('An error occurred getting request_token from Twitter')
-request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
-print('Please fallow the site  in your browser')
-print('{}?oauth_token={}'.format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
-
-oauth_verifier = input('please enter the pin\n')
-token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
-token.set_verifier(oauth_verifier)
-client = oauth2.Client(consumer, token)
-response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
-access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
-print(access_token)
-email = input('Enter your email please:\n')
-first_name = input('now first name\n')
-last_name = input('and the last name:\n')
-
-user = User(email, first_name, last_name, access_token['oauth_token'], access_token['oauth_token_secret'], None)
 Database.initialise_connection(
-            database='Learning',
-            user='postgres',
-            password='karenina',
-            host='localhost',
-            port=5433)
-user.save_to_db()
-authorized_token = oauth2.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
-authorized_client = oauth2.Client(consumer, authorized_token)
+    database='Learning',
+    user='postgres',
+    password='karenina',
+    host='localhost',
+    port=5433)
+user_email = input('enter your email: \n')
+user = User.load_from_db(user_email)
+if user:
+    print(User.load_oauth(user_email))
+else:
 
-response, content = authorized_client.request(
-    'https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images',
-    'GET')
-if response.status != 200:
-    print('An error has occurred')
-tweets = json.loads(content.decode('utf-8'))
-pprint(tweets['statuses'])
-print(list(s['text'] for s in tweets['statuses']))
+    consumer = oauth2.Consumer(constants.CLIENT_KEY, constants.CLIENT_SECRET)
+    client = oauth2.Client(consumer)
+    response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
+    if response.status != 200:
+        print('An error occurred getting request_token from Twitter')
+    request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+    print('Please fallow the site  in your browser')
+    print('{}?oauth_token={}'.format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
+
+    oauth_verifier = input('please enter the pin\n')
+    token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+    token.set_verifier(oauth_verifier)
+    client = oauth2.Client(consumer, token)
+    response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+    access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+    print(access_token)
+
+    email = input('Enter your email please:\n')
+    first_name = input('now first name\n')
+    last_name = input('and the last name:\n')
+    user= User(email, first_name, last_name, access_token['oauth_token'], access_token['oauth_token_secret'], None)
+    user.save_to_db()
+
+    authorized_token = oauth2.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
+    authorized_client = oauth2.Client(consumer, authorized_token)
+
+    response, content = authorized_client.request(
+        'https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images',
+        'GET')
+    print(response)
+    if response.status != 200:
+        print('An error has occurred')
+    tweets = json.loads(content.decode('utf-8'))
+    pprint(tweets['statuses'])
+    print(list(s['text'] for s in tweets['statuses']))
--- Python -Web-and-APIs/Twitter/user.py
+++ Python -Web-and-APIs/Twitter/user.py
@@ -1,3 +1,8 @@
+import json
+
+import oauth2
+
+from constants import CLIENT_SECRET, CLIENT_KEY
 from database import GetConnectionFromPool
 
 
@@ -20,11 +25,31 @@ class User:
                            (self.email, self.first_name, self.last_name, self.oauth_token, self.oauth_token_secret))
 
     @classmethod
+    def load_oauth(cls, email):
+        with GetConnectionFromPool() as cursor:
+            cursor.execute('select oauth_token, oauth_token_secret from users Where email = %s', (email,))
+            data = cursor.fetchone()
+            if data:
+                print(data[0], data[1])
+                authorized_token = oauth2.Token(data[0], data[1])
+                authorized_client = oauth2.Client(oauth2.Consumer(CLIENT_KEY, CLIENT_SECRET), authorized_token)
+                response, content = authorized_client.request(
+                     'https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images',
+                     'GET')
+                if response.status != 200:
+                    print('oops')
+                else:
+                    tweets = json.loads(content.decode('utf-8'))
+                    return tweets['statuses']
+
+    @classmethod
     def load_from_db(cls, email):
         with GetConnectionFromPool() as cursor:
             cursor.execute('Select* from users where Email = %s', (email,))
             data = cursor.fetchone()
-        print(cls(email=data[1], first_name=data[2], last_name=data[3], oauth_token=data[4], oauth_token_secret=data[5],
-                  id=data[0]))
-        return ' Email- {}\n First Name- {}\n Last Name- {}\n Oauth Token- {}\n Oauth Token Secret {}\nID- {}'.format(
-            data[1], data[2], data[3], data[4], data[5], data[0])
+            if data:
+                print(cls(email=data[1], first_name=data[2], last_name=data[3], oauth_token=data[4],
+                          oauth_token_secret=data[5],
+                          id=data[0]))
+                return ' Email- {}\n First Name- {}\n Last Name- {}\n Oauth Token- {}\n Oauth Token Secret {}\nID- {}'.format(
+                    data[1], data[2], data[3], data[4], data[5], data[0])
